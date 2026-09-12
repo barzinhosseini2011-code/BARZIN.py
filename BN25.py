5da8ac71d8722e2bfdd51637f439f7157f4270e7
barzinhosseini2011-code <barzinhosseini2011@gmail.com>
2026-09-12 10:44:38 +0330

    Add files via upload

---
+++ BN25.py
@@ -0,0 +1,91 @@
+import turtle 
+
+turtle.speed(5)
+
+win = turtle.Screen()
+win.title("BaRzIn")
+win.bgcolor("#2FD6AF")
+
+
+turtle.bgcolor("#2F50D6")
+
+
+#B
+turtle.goto(-400, 0)
+turtle.right(270)
+turtle.forward(100)
+for i in range(4):
+    turtle.forward(40)
+    turtle.right(90)
+turtle.right(180)
+turtle.forward(25)
+for i in range(4):
+    turtle.forward(40)
+    turtle.right(270)
+#.
+turtle.forward(75)
+turtle.right(270)
+turtle.forward(80)
+#ََA
+turtle.right(270)
+turtle.forward(75)
+for i in range(4):
+    turtle.forward(40)
+    turtle.right(90)
+turtle.right(90)
+turtle.forward(40)
+turtle.right(90)
+
+turtle.forward(75)
+#.
+turtle.right(270)
+turtle.forward(60)
+#R
+turtle.right(270)
+turtle.forward(75)
+for i in range(4):
+    turtle.forward(40)
+    turtle.right(90)
+turtle.right(135)
+turtle.forward(106)
+#.
+
+turtle.right(315)
+turtle.forward(60)
+#Z
+
+turtle.right(315)
+turtle.forward(110)
+turtle.right(225)
+turtle.forward(106)
+turtle.right(180)
+turtle.forward(106)
+turtle.right(135)
+turtle.forward(110)
+turtle.right(225)
+turtle.forward(106)
+#.
+turtle.forward(40)
+#I
+
+turtle.right(270)
+turtle.forward(106)
+#.
+turtle.right(180)
+turtle.forward(106)
+turtle.right(270)
+turtle.forward(160)
+turtle.right(180)
+turtle.forward(100)
+
+#N
+turtle.right(90)
+turtle.forward(106)
+turtle.right(155)
+turtle.forward(116)
+turtle.right(205)
+turtle.forward(106)
+
+
+turtle.done()
+
